21c3ee2666b4a5d749be45b3f3fb8b36360047ef
Neliuswandia <nelius.wandia@student.moringaschool.com>
2025-03-25 16:14:24 +0300

    added a number to my list

--- list.py
+++ list.py
@@ -1,5 +1,11 @@
 my_list = []
 
-my_list.append([10, 20, 30, 40])
+new_list = [10, 20, 30, 40]
+
+for x in new_list:
+    my_list.append(x)
+
+# my_list.append([10, 20, 30, 40])
+my_list.insert(1, 15)
 
 print(my_list)
