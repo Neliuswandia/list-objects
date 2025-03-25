6f919adde6fc01fc746e21df9632be510c57e366
Neliuswandia <nelius.wandia@student.moringaschool.com>
2025-03-25 16:20:17 +0300

    extended my list

--- list.py
+++ list.py
@@ -1,11 +1,15 @@
+# empty list 
 my_list = []
-
+ 
+#  appending into a list
 new_list = [10, 20, 30, 40]
 
 for x in new_list:
     my_list.append(x)
 
-# my_list.append([10, 20, 30, 40])
+# inserting into a list as per the index
 my_list.insert(1, 15)
+
+my_list.extend([50,60,70])
 
 print(my_list)
