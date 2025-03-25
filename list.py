4f3126c11a9a07d441c4c069c87c096cc311780e
Neliuswandia <nelius.wandia@student.moringaschool.com>
2025-03-25 16:33:13 +0300

    printing the value of a certain index from my list

--- list.py
+++ list.py
@@ -16,6 +16,10 @@ my_list.extend([50,60,70])
 # removing the last element
 my_list.pop()
 
+# sorting my list in an ascending order
 my_list.sort(reverse=True)
+
+# printing the value of a given index from my list
+print(my_list[3])
 
 print(my_list)
